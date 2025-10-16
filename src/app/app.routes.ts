dd50101e1352d141bb3cde5c8167ee39a138a5de
diego <diegosorianomagallon@gmail.com>
2025-10-16 19:08:03 +0200

    experience page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
 import { HomePage } from './pages/home-page/home-page';
+import { ExperiencePage } from './pages/experience-page/experience-page';
 
 export const routes: Routes = [
     { path: '', component: HomePage },
+    { path: 'experience/:company', component: ExperiencePage}
 ];
--- src/app/components/experience-component/experience-component.ts
+++ src/app/components/experience-component/experience-component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import { WorkExperience, WorkExperienceData } from '../../data/experience-data';
+import { RouterLink } from '@angular/router';
+import { phosphorArrowUpRight } from '@ng-icons/phosphor-icons/regular'
 
 @Component({
   selector: 'app-experience-component',
-  imports: [],
+  imports: [RouterLink, NgIcon],
   templateUrl: './experience-component.html',
-  styleUrl: './experience-component.css'
+  styleUrl: './experience-component.css',
+  viewProviders: provideIcons({ phosphorArrowUpRight })
 })
 export class ExperienceComponent {
   workExperiencie: WorkExperience[] = WorkExperienceData;
